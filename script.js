36b9c0a14a558d2ee1ca5489185eda0022c2f245
Sophie <sophiedodsworth@hotmail.com>
2023-01-07 19:59:53 +0000

    added alert if user doesn't select any characters for password

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ var upperCasedCharacters = [
 // Function to generate password with user input
 function generatePassword() {
 
-  // Prompt asking for user input to choose how many characters for the password
+  // Prompt asking for user input to choose how many characters for the password & ensuring the user enters a number value
   let passwordLength = prompt("How many characters would you like for your password? You may choose between 10 and 64.");
   console.log(passwordLength);
   console.log(/^\d+$/.test(passwordLength))
@@ -106,62 +106,61 @@ function generatePassword() {
     // Ensuring the user can't proceed if they press cancel - if they do then an alert will be displayed and the prompt will be asked again
   } else if (!passwordLength) {
     alert("A number must be entered for your password length.");
-  generatePassword()
+    generatePassword()
     // If the above criteria is met and the user input is valid, it will then proceed to confirm to the user how many characters they've chosen for their password 
   } else {
     alert(`Your random generated password will have ${passwordLength} characters.`);
     let newPassword = "";
 
-  // Asking the user to confirm which characters they'd like to use for their password
-  let confirmSpecialCharacters = confirm("Would you like your password to contain special characters?");
-  let confirmNumericCharacters = confirm("Would you like your password to contain numbers?");
-  let confirmLowerCasedCharacters = confirm("Would you like your password to contain lowercase characters?");
-  let confirmUpperCasedCharacters = confirm("Would you like your password to contain uppercase characters?");
-  console.log(confirmSpecialCharacters, confirmLowerCasedCharacters)
+    // Asking the user to confirm which characters they'd like to use for their password
+    let confirmSpecialCharacters = confirm("Would you like your password to contain special characters?");
+    let confirmNumericCharacters = confirm("Would you like your password to contain numbers?");
+    let confirmLowerCasedCharacters = confirm("Would you like your password to contain lowercase characters?");
+    let confirmUpperCasedCharacters = confirm("Would you like your password to contain uppercase characters?");
+    console.log(confirmSpecialCharacters, confirmLowerCasedCharacters)
 
-  // If none of the characters are confirmed by the user then it will alert them to their error and ask them to confirm at least one in order to generate password - if it happens again it will then loop
-//   if (confirmUpperCasedCharacters === false && confirmLowerCasedCharacters === false && confirmSpecialCharacters === false && confirmNumericCharacters === false) {
-//     alert("You must choose at least one of the criteria to generate a password.");
-// generatePassword()
-//   }
+    // If none of the characters are confirmed by the user then it will alert them to their error and ask them to confirm at least one in order to generate password - if it happens again it will then loop
+    if (confirmUpperCasedCharacters === false && confirmLowerCasedCharacters === false && confirmSpecialCharacters === false && confirmNumericCharacters === false) {
+      alert("You must choose at least one of the criteria to generate a password.");
+      generatePassword()
+    }
 
-  // Creating a new array 
-  var characterCombination = [];
+    // Creating a new array 
+    var characterCombination = [];
 
-  // Concatenating the special characters array onto the new array if the user selects it
-  if (confirmSpecialCharacters) {
-    console.log("Inside special character")
-    characterCombination = characterCombination.concat(specialCharacters)
+    // Concatenating the special characters array onto the new array if the user selects it
+    if (confirmSpecialCharacters) {
+      characterCombination = characterCombination.concat(specialCharacters)
+    }
+    //  Concatenating the numeric characters array onto the new array if the user selects it
+    if (confirmNumericCharacters) {
+      characterCombination = characterCombination.concat(numericCharacters)
+    }
+
+    // Concatenating the lower cased characters array onto the new array if the user selects it
+    if (confirmLowerCasedCharacters) {
+      characterCombination = characterCombination.concat(lowerCasedCharacters)
+    }
+
+    // Concatenating the upper cased characters array onto the new array if the user selects it
+    if (confirmUpperCasedCharacters) {
+      characterCombination = characterCombination.concat(upperCasedCharacters)
+    }
+
+    // Creating an empty string for the password that will be generated based on the for loop below
+
+    // For loop selecting random characters from the new array which is concatenated with whichever characters the user selected
+    for (var i = 0; i < passwordLength; i++) {
+      newPassword = newPassword + characterCombination[Math.floor(Math.random() * characterCombination.length)];
+    }
+    console.log(newPassword)
+
+    // Telling it that the string which now contains the characters selected by the user should be returned in replacement of the 'Your Secure Password' placeholder
+    var passwordText = document.querySelector('#password');
+    passwordText.value = ""
+
+    passwordText.value = newPassword;
   }
-  //  Concatenating the numeric characters array onto the new array if the user selects it
-  if (confirmNumericCharacters) {
-    characterCombination = characterCombination.concat(numericCharacters)
-  }
-
-  // Concatenating the lower cased characters array onto the new array if the user selects it
-  if (confirmLowerCasedCharacters) {
-    characterCombination = characterCombination.concat(lowerCasedCharacters)
-  }
-
-  // Concatenating the upper cased characters array onto the new array if the user selects it
-  if (confirmUpperCasedCharacters) {
-    characterCombination = characterCombination.concat(upperCasedCharacters)
-  }
-
-  // Creating an empty string for the password that will be generated based on the for loop below
-
-  // For loop selecting random characters from the new array which is concatenated with whichever characters the user selected
-  for (var i = 0; i < passwordLength; i++) {
-    newPassword = newPassword + characterCombination[Math.floor(Math.random() * characterCombination.length)];
-  }
-  console.log(newPassword)
-
-  // Telling it that the string which now contains the characters selected by the user should be returned in replacement of the 'Your Secure Password' placeholder
-  var passwordText = document.querySelector('#password');
-  passwordText.value = ""
-
-  passwordText.value = newPassword;
-}
 
 };
 
@@ -169,11 +168,11 @@ function generatePassword() {
 var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   console.log(password)
+function writePassword() {
+  var password = generatePassword();
+  console.log(password)
 
-// }
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', generatePassword);
