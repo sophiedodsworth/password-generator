ee0de2a359cc688d1e4abf9d53c9a50f1628a1c1
Sophie <sophiedodsworth@hotmail.com>
2023-01-08 11:04:41 +0000

    tidied code and added clarifying text to prompts

--- script.js
+++ script.js
@@ -110,19 +110,20 @@ function generatePassword() {
     // If the above criteria is met and the user input is valid, it will then proceed to confirm to the user how many characters they've chosen for their password 
   } else {
     alert(`Your random generated password will have ${passwordLength} characters.`);
+    
     let newPassword = "";
 
     // Asking the user to confirm which characters they'd like to use for their password
-    let confirmSpecialCharacters = confirm("Would you like your password to contain special characters?");
-    let confirmNumericCharacters = confirm("Would you like your password to contain numbers?");
-    let confirmLowerCasedCharacters = confirm("Would you like your password to contain lowercase characters?");
-    let confirmUpperCasedCharacters = confirm("Would you like your password to contain uppercase characters?");
-    console.log(confirmSpecialCharacters, confirmLowerCasedCharacters)
+    let confirmSpecialCharacters = confirm("Would you like your password to contain special characters? Click OK to confirm.");
+    let confirmNumericCharacters = confirm("Would you like your password to contain numbers? Click OK to confirm.");
+    let confirmLowerCasedCharacters = confirm("Would you like your password to contain lowercase characters? Click OK to confirm.");
+    let confirmUpperCasedCharacters = confirm("Would you like your password to contain uppercase characters? Click OK to confirm.");
+    console.log(confirmSpecialCharacters, confirmLowerCasedCharacters, confirmNumericCharacters, confirmUpperCasedCharacters)
 
-    // If none of the characters are confirmed by the user then it will alert them to their error and ask them to confirm at least one in order to generate password - if it happens again it will then loop
+    // If none of the characters are confirmed by the user then it will alert them to their error and ask them to confirm at least one in order to generate password 
     if (confirmUpperCasedCharacters === false && confirmLowerCasedCharacters === false && confirmSpecialCharacters === false && confirmNumericCharacters === false) {
-      alert("You must choose at least one of the criteria to generate a password.");
-      generatePassword()
+      alert("You must choose at least one of the criteria to generate a password. Please refresh the page and try again.");
+      console.log("A password could not be generated without any of the criteria selected.")
     }
 
     // Creating a new array 
@@ -147,19 +148,18 @@ function generatePassword() {
       characterCombination = characterCombination.concat(upperCasedCharacters)
     }
 
-    // Creating an empty string for the password that will be generated based on the for loop below
-
     // For loop selecting random characters from the new array which is concatenated with whichever characters the user selected
     for (var i = 0; i < passwordLength; i++) {
       newPassword = newPassword + characterCombination[Math.floor(Math.random() * characterCombination.length)];
     }
-    console.log(newPassword)
+    // console.log(newPassword)
 
     // Telling it that the string which now contains the characters selected by the user should be returned in replacement of the 'Your Secure Password' placeholder
     var passwordText = document.querySelector('#password');
-    passwordText.value = ""
+    passwordText.value = "";
 
     passwordText.value = newPassword;
+
   }
 
 };
